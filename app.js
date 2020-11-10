93c2dc91416cf14c1fd102948fdfc9e35905d64d
Thomas Sten <eric@Thomass-MBP.fios-router.home>
2020-11-10 14:02:14 -0500

    homepage load function works

--- app.js
+++ app.js
@@ -3,15 +3,63 @@ console.log('Yo Dawg')
   // create the main page with a function that runs on load
   // function will append the main html section with and Image and a button
   // button takes user input (click) to move on to the next section of the app
+const mainPage = document.querySelector('.main')
+const homePageImage = 'https://i.imgur.com/aQ7nfhC.jpg'
+let onLoad = () => {
+  let home = document.createElement('div')
+  home.className = ('home')
+  mainPage.append(home)
+  let homeImage = document.createElement('img')
+  homeImage.setAttribute('src', homePageImage)
+  home.append(homeImage)
+  let begin = document.createElement('input')
+  begin.setAttribute('type', 'submit')
+  begin.setAttribute('value', 'Click to Create a Character')
+  home.append(begin)
+}
+  onLoad()
+
 //charecter
   //set a variable to charecter which will be an object
   // charecter will include a race and class
   // charecter = object
   //      race
   //      class
+  // create an array of races
+  // create an array of classes
+  // create an image bank for each class
+  
+
 // clear the main
   //function to append the main and remove child elements
   //function = clearMain
+
 // User selects a race
     // on mainpage button event
       // run clearMain function
+    //in a function:
+      // append main html to present all races as options
+      // add event listeners for user to click to select a race, a checkmark works as well
+      //create a button on the main html that advances to the next section
+      // on click store the users race selection in the character object and advance to next section of app
+
+// user selects a class
+   // on advance button event
+      // run clearMain function
+    //in a function:
+      // append main html to present all classes as options
+      // add event listeners for user to click to select a class, a checkmark works as well
+      //create a button on the main html that advances to the next section
+      // on click store the users class selection in the character object and advance to next section of app
+
+// display results
+  // on advance button event
+      // run clearMain function
+  //in a function
+    // append the main html to display the race and class chosen by the user as text at the top of the page below the heading
+    // access the image bank for the corresponding class and append to the main to appear on the right
+    // use character class to access Dnd API
+      // dispay the classes proficiencies and attributes listed on the API appended to the main on the left of the image
+    // Append main with a start over button
+      // button when click will refresh page or invoke main page function
+
