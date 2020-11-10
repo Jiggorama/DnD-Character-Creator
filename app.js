bd17794716cf7e6f2d89dc619ad6acd03f6b8556
Thomas Sten <eric@Thomass-MBP.fios-router.home>
2020-11-10 12:58:29 -0500

    My Garnet Project 1

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('Yo Dawg')
+//main page on load
+  // create the main page with a function that runs on load
+  // function will append the main html section with and Image and a button
+  // button takes user input (click) to move on to the next section of the app
+//charecter
+  //set a variable to charecter which will be an object
+  // charecter will include a race and class
+  // charecter = object
+  //      race
+  //      class
+// clear the main
+  //function to append the main and remove child elements
+  //function = clearMain
+// User selects a race
+    // on mainpage button event
+      // run clearMain function
